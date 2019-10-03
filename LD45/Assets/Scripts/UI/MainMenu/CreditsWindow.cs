c85ad12ae05bca9de40dc3b63c3c7b31eb5d9a31
Team-on <timonsol13@gmail.com>
2019-10-03 22:11:47 +0300

    Windows templates

---
+++ LD45/Assets/Scripts/UI/MainMenu/CreditsWindow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsWindow : BaseWindow {
+	new void Awake() {
+		base.Awake();
+	}
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
---
+++ LD45/Assets/Scripts/UI/MainMenu/SettingsWindow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsWindow : BaseWindow {
+	new void Awake() {
+		base.Awake();
+	}
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
