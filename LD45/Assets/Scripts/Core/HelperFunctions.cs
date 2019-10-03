43b25f74d1f34d6a2c7c0e61ffb9aa384794c0ef
Team-on <timonsol13@gmail.com>
2019-10-03 19:58:07 +0300

    Fix OutOfRangeEx bug

--- LD45/Assets/Scripts/Core/HelperFunctions.cs
+++ LD45/Assets/Scripts/Core/HelperFunctions.cs
@@ -31,6 +31,6 @@ public static class HelperFunctions {
 	}
 
 	public static T Random<T>(this IList<T> list) {
-		return list[UnityEngine.Random.Range(0, list.Count + 1)];
+		return list[UnityEngine.Random.Range(0, list.Count)];
 	}
 }
