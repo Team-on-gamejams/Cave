330e9865a54771d0c423c77f53d28ad1244734ef
Team-on <timonsol13@gmail.com>
2019-10-04 01:56:08 +0300

    Add click sound

---
+++ LD45/Assets/Scripts/Core/SoundManager/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager> {
+	const byte maxSimultaneouslySounds = 5;
+	AudioSource[] sounds = new AudioSource[maxSimultaneouslySounds];
+
+	protected SoundManager() { }
+
+	void Awake() {
+		for(byte i = 0; i < maxSimultaneouslySounds; ++i) {
+			GameObject soundgo = new GameObject();
+			soundgo.transform.parent = transform;
+			soundgo.name = $"sounds[{i}]";
+			sounds[i] = soundgo.AddComponent<AudioSource>();
+		}
+	}
+
+	public void PlaySound(AudioClip clip) {
+		sounds[0].PlayOneShot(clip);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LD45
 Ludum Dare Jam #45. Theme: "--------". Game: -----------
+
+Click sound: https://freesound.org/people/moogy73/sounds/425728/
