2c5c8cfdaabe8d3d25eac4c21e71e484a81c8d67
Team-on <timonsol13@gmail.com>
2019-10-04 16:34:58 +0300

    Add music to sound manager

--- LD45/Assets/Scripts/Core/SoundManager/SoundManager.cs
+++ LD45/Assets/Scripts/Core/SoundManager/SoundManager.cs
@@ -3,21 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : Singleton<SoundManager> {
-	const byte maxSimultaneouslySounds = 5;
-	AudioSource[] sounds = new AudioSource[maxSimultaneouslySounds];
+	const float musicChangeTime = 1.0f;
+
+	public float MasterVolume { get; set; }
+	public float MusicVolume { get; set; }
+	public float SoundVolume { get; set; }
+
+	AudioSource musicSource;
+	AudioSource soundSource;
+	Coroutine changeMusicCoroutine;
 
 	protected SoundManager() { }
 
 	void Awake() {
-		for(byte i = 0; i < maxSimultaneouslySounds; ++i) {
+		//TODO: save/load this values
+		MasterVolume = MusicVolume = SoundVolume = 100;
+
+		soundSource = CreateAS("soundSource");
+		soundSource.volume = SoundVolume * MasterVolume;
+
+		musicSource = CreateAS("musicSource");
+		musicSource.volume = MusicVolume * MasterVolume;
+
+		AudioSource CreateAS(string name) {
 			GameObject soundgo = new GameObject();
 			soundgo.transform.parent = transform;
-			soundgo.name = $"sounds[{i}]";
-			sounds[i] = soundgo.AddComponent<AudioSource>();
+			soundgo.name = name;
+			return soundgo.AddComponent<AudioSource>();
 		}
 	}
 
 	public void PlaySound(AudioClip clip) {
-		sounds[0].PlayOneShot(clip);
+		soundSource.PlayOneShot(clip);
+	}
+
+	public void PlayMusic(AudioClip music) {
+		changeMusicCoroutine = StartCoroutine(ChangeMusic());
+
+		IEnumerator ChangeMusic() {
+			float timer = musicChangeTime;
+			float startMusicVolume = musicSource.volume;
+
+			while ((timer -= Time.deltaTime) > 0) {
+				musicSource.volume = startMusicVolume * timer / musicChangeTime;
+				yield return null;
+			}
+
+			yield return null;
+			musicSource.Stop();
+			yield return null;
+			musicSource.volume = MusicVolume * MasterVolume;
+			musicSource.clip = music;
+			musicSource.Play();
+		}
 	}
 }
