762acf3ec32f4a2bfac6da1876c8e161cd83f2d5
Team-on <timonsol13@gmail.com>
2019-10-05 13:00:45 +0300

    Add stat template

---
+++ LD45/Assets/Scripts/Game/Character/BaseStat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseStat{
+	public float MaxValue {
+		get {
+			return _maxValue;
+		}
+		set {
+			_maxValue = value;
+		}
+	}
+	public float Value {
+		get {
+			return _value;
+		}
+		set {
+			_value = value;
+			if (_value > MaxValue)
+				_value = MaxValue;
+			if (_value < 0)
+				_value = 0;
+		}
+	}
+
+	[SerializeField] float _maxValue;
+	[SerializeField] float _value;
+
+	public BaseStat() {
+		_value = _maxValue = 0;
+	}
+
+	public void ChangeBy(float val) {
+		Value += val;
+	}
+}
---
+++ LD45/Assets/Scripts/Game/Character/Character.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+	public BaseStat HitPoints;
+	public BaseStat SleepPoints;
+	public BaseStat HungerPoints;
+}
