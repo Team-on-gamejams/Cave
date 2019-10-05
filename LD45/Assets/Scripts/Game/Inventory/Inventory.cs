fd53a25bd780b4fbb5a7a04a45163b8210193a9e
Team-on <timonsol13@gmail.com>
2019-10-05 14:41:49 +0300

    Inventory interfaces

---
+++ LD45/Assets/Scripts/Game/Inventory/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+	public List<ItemSO> items;
+
+	void Awake() {
+		items = new List<ItemSO>();
+	}
+
+	public void AddItem(ItemSO item) {
+		//Add to Count
+		items.Add(item);
+	}
+
+	public void RemoveItem(ItemSO item) {
+		//Remove to Count
+		items.Remove(item);
+	}
+}
--- LD45/Assets/Scripts/Game/Items/ItemSO.cs
+++ LD45/Assets/Scripts/Game/Items/ItemSO.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "New ItemSO", menuName = "ItemSO", order = 51)]
 public class ItemSO : ScriptableObject {
-	public string name;
+	public string Name;
+	public string Description;
+	public byte Count;
+	public byte MaxCount;
+	public Texture2D Texture;
 }
