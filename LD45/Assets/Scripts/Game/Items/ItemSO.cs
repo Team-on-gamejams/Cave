ae2c735a5cece6b048450dc1377b6a0aec391e98
Team-on <timonsol13@gmail.com>
2019-10-05 12:39:23 +0300

    Add BaseItem

---
+++ LD45/Assets/Scripts/Game/Items/ItemSO.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ItemSO", menuName = "ItemSO", order = 51)]
+public class ItemSO : ScriptableObject {
+	public string name;
+}
