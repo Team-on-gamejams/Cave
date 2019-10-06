ae11655f4fb56b4d1995606812edec08be67e74f
Sasha <41016761+FeltMe@users.noreply.github.com>
2019-10-07 02:57:16 +0300

    +Zombie and movement

--- LD45/Assets/Scripts/Game/Character/Character.cs
+++ LD45/Assets/Scripts/Game/Character/Character.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public abstract class Character : MonoBehaviour {
 	public BaseStat HitPoints;
-	public BaseStat SleepPoints;
-	public BaseStat HungerPoints;
 
 	public float speed;
 }
--- LD45/Assets/Scripts/Game/Character/Enemy.cs
+++ LD45/Assets/Scripts/Game/Character/Enemy.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Character {
-	// Start is called before the first frame update
-	void Start() {
+public class Enemy : Character
+{
+    private Rigidbody2D rigidbody;
 
-	}
+    public void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
 
-	// Update is called once per frame
-	void Update() {
+    private void FixedUpdate()
+    {
+        var playerpos = GameManager.Instance.Player.transform.position;
+        var mobpos = transform.position;
+        if((playerpos - mobpos).sqrMagnitude < 4 && (playerpos - mobpos).sqrMagnitude > 1)
+        {
+            rigidbody.MovePosition(((playerpos - mobpos).normalized * speed * Time.deltaTime) + transform.localPosition);
+        }
+    }
 
-	}
 }
