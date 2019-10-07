bd0a9b0906b422190a2a8a1f5e2ebb538f1b5ed8
Team-on <timonsol13@gmail.com>
2019-10-07 03:08:56 +0300

    Return deleted stats

--- LD45/Assets/Scripts/Game/Character/Character.cs
+++ LD45/Assets/Scripts/Game/Character/Character.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public abstract class Character : MonoBehaviour {
 	public BaseStat HitPoints;
+	public BaseStat StaminaPoints;
+	public BaseStat ManaPoints;
+	public BaseStat SleepPoints;
+	public BaseStat HungerPoints;
 
 	public float speed;
 }
