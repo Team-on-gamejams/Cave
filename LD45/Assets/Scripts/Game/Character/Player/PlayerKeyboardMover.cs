6f2bdba5c1a894421655218de81fa368fa88f43e
Team-on <timonsol13@gmail.com>
2019-10-14 17:52:39 +0300

    Buttons down sometimes ignored

--- LD45/Assets/Scripts/Game/Character/Player/PlayerKeyboardMover.cs
+++ LD45/Assets/Scripts/Game/Character/Player/PlayerKeyboardMover.cs
@@ -9,8 +9,7 @@ public class PlayerKeyboardMover : MonoBehaviour {
 	public bool CanMouseMove = true;
 	public Action OnMouseMoveEnd;
 
-	public KeyCode[] Keys;
-	public UnityEvent[] OnButtonPressed;
+	public InputEvent[] Inputs;
 
 	[SerializeField] SpriteRenderer spriteRenderer;
 	[SerializeField] Rigidbody2D rigidbody;
@@ -28,65 +27,74 @@ public class PlayerKeyboardMover : MonoBehaviour {
 		animator = player.Animator;
 	}
 
-	//Split to different methods
-	void FixedUpdate() {
-		//TODO: початок руху з прискоренням вплоть до макс. швидкості. Зупинка моментальна
-		if (!player.IsPlayingBlockerAnimation) {
-			float v = Input.GetAxisRaw("Vertical");
-			float h = Input.GetAxisRaw("Horizontal");
-			bool wasMoved = false;
+	void Update() {
+		for (byte i = 0; i < Inputs.Length; ++i) {
+			if (Input.GetKeyDown(Inputs[i].Key)) {
+				Inputs[i].OnButtonPressed?.Invoke();
+				if (Inputs[i].InterruptAnim)
+					player.IsPlayingBlockerAnimation = true;
+			}
+		}
+	}
 
-			if (v != 0 || h != 0) {
-				if (moveToPoint != Vector3.zero) {
-					moveToPoint = Vector3.zero;
+	void FixedUpdate() {
+		float v = Input.GetAxisRaw("Vertical");
+		float h = Input.GetAxisRaw("Horizontal");
+		bool wasMoved = false;
+
+		if (v != 0 || h != 0)
+			WASDMove(v, h, ref wasMoved);
+
+		if (CanMouseMove && Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+			MouseMove(Input.mousePosition, ref wasMoved);
+
+		ProcessMove(ref wasMoved);
+
+	}
+
+	void WASDMove(float v, float h, ref bool wasMoved) {
+		if (moveToPoint != Vector3.zero) {
+			moveToPoint = Vector3.zero;
+			OnMouseMoveEnd = null;
+		}
+		rigidbody.MovePosition(transform.localPosition + new Vector3(h, v).normalized * player.speed * Time.deltaTime);
+		wasMoved = true;
+
+		if ((h > 0 && transform.localScale.x < 0) || (h < 0 && transform.localScale.x > 0))
+			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z); ;
+	}
+
+	void MouseMove(Vector3 mousePos, ref bool wasMoved) {
+		moveToPoint = GameManager.Instance.MainCamera.ScreenToWorldPoint(mousePos);
+		moveToPoint.z = 0;
+		moveToDirection = moveToPoint - transform.position;
+
+		if ((moveToDirection.x > 0 && transform.localScale.x < 0) || (moveToDirection.x < 0 && transform.localScale.x > 0))
+			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+	}
+
+	void ProcessMove(ref bool wasMoved) {
+		if (moveToPoint != Vector3.zero) {
+			if ((transform.position - moveToPoint).sqrMagnitude > GameManager.Instance.Player.InteractDistSqr) {
+				rigidbody.MovePosition(transform.localPosition + moveToDirection.normalized * player.speed * Time.deltaTime);
+				wasMoved = true;
+			}
+			else {
+				moveToPoint = Vector3.zero;
+				if (OnMouseMoveEnd != null) {
+					OnMouseMoveEnd.Invoke();
 					OnMouseMoveEnd = null;
 				}
-				rigidbody.MovePosition(transform.localPosition + new Vector3(h, v).normalized * player.speed * Time.deltaTime);
-				wasMoved = true;
-				//if (h > 0 && spriteRenderer.flipX)
-				//	spriteRenderer.flipX = false;
-				//if (h < 0 && !spriteRenderer.flipX)
-				//	spriteRenderer.flipX = true;
-
-				if ((h > 0 && transform.localScale.x < 0) || (h < 0 && transform.localScale.x > 0))
-					transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z); ;
 			}
-
-			if (CanMouseMove && Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) {
-				moveToPoint = GameManager.Instance.MainCamera.ScreenToWorldPoint(Input.mousePosition);
-				moveToPoint.z = 0;
-				moveToDirection = moveToPoint - transform.position;
-
-				if ((moveToDirection.x > 0 && transform.localScale.x < 0) || (moveToDirection.x < 0 && transform.localScale.x > 0))
-					transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z); ;
-			}
-
-			if (moveToPoint != Vector3.zero) {
-				if ((transform.position - moveToPoint).sqrMagnitude > GameManager.Instance.Player.InteractDistSqr) {
-					rigidbody.MovePosition(transform.localPosition + moveToDirection.normalized * player.speed * Time.deltaTime);
-					wasMoved = true;
-				}
-				else {
-					moveToPoint = Vector3.zero;
-					if (OnMouseMoveEnd != null) {
-						OnMouseMoveEnd.Invoke();
-						OnMouseMoveEnd = null;
-					}
-				}
-			}
-
-			animator.SetBool("IsMoving", wasMoved);
-		
-			for (byte i = 1; i < Keys.Length; ++i)
-				if (Input.GetKeyDown(Keys[i]))
-					OnButtonPressed[i]?.Invoke();
 		}
 
-		//TODO: Самий справжній костиль
-		//Переробити на інпут в якого буде буль - чи можна робити під час анімації
-		//Бо щас під час анімацій можна лише відкривати інвентарь
-		//Того щас такий костиль
-		if (Input.GetKeyDown(Keys[0]))
-			OnButtonPressed[0]?.Invoke();
+		animator.SetBool("IsMoving", wasMoved);
 	}
+}
+
+[Serializable]
+public class InputEvent {
+	public KeyCode Key;
+	public bool InterruptAnim;
+	public UnityEvent OnButtonPressed;
 }
