6126eb51f3dcc7e572656c011caf959b5a7622ad
Team-on <timonsol13@gmail.com>
2019-10-17 03:23:28 +0300

    Add .zip archivation to build pipeline

--- LD45/Assets/Scripts/Core/Editor/BuildPipeline/BuildManager.cs
+++ LD45/Assets/Scripts/Core/Editor/BuildPipeline/BuildManager.cs
@@ -6,9 +6,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
+using Ionic.Zip;
 
-//TODO: archivate build
-//TODO: publish to itch.io
+//TODO: publish to itch.io via butler
 public static class BuildManager {
 	public static string LastBundleVersion {
 		get {
@@ -37,18 +37,17 @@ public static class BuildManager {
 	static string _LastBundleVersion = null;
 	static int _LastBuildPatch = -1;
 
-	public static void BuildAll() {
+	public static void BuildAll(bool needZip) {
 		Debug.Log("Start building all");
 		DateTime startTime = DateTime.Now;
 		BuildTarget targetBeforeStart = EditorUserBuildSettings.activeBuildTarget;
 		BuildTargetGroup targetGroupBeforeStart = BuildPipeline.GetBuildTargetGroup(targetBeforeStart);
 
-		BuildWindows(true);
-		BuildWindowsX64(true);
-		BuildLinux(true);
-		BuildOSX(true);
-		//TODO: incoment after web build fix
-		//BuildWeb(true);
+		BuildWindows(true, needZip);
+		BuildWindowsX64(true, needZip);
+		BuildLinux(true, needZip);
+		BuildOSX(true, needZip);
+		BuildWeb(true, needZip);
 
 		++LastBuildPatch;
 
@@ -57,57 +56,63 @@ public static class BuildManager {
 		Debug.Log($"End building all. Elapsed time: {string.Format("{0:mm\\:ss}", DateTime.Now - startTime)}");
 	}
 
-	public static void BuildWindows(bool isInBuildSequence) {
+	public static void BuildWindows(bool isInBuildSequence, bool needZip) {
 		BaseBuild(
 			BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows,
 			isInBuildSequence ? BuildOptions.None : BuildOptions.ShowBuiltPlayer,
 			!isInBuildSequence,
 			!isInBuildSequence,
+			needZip ? "_Windows" : null,
 			$"_Windows/{PlayerSettings.productName}.exe"
 		);
 	}
 
-	public static void BuildWindowsX64(bool isInBuildSequence) {
+	public static void BuildWindowsX64(bool isInBuildSequence, bool needZip) {
 		BaseBuild(
 			BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64,
 			isInBuildSequence ? BuildOptions.None : BuildOptions.ShowBuiltPlayer,
 			!isInBuildSequence,
 			!isInBuildSequence,
+			needZip ? "_Windows64" : null,
 			$"_Windows64/{PlayerSettings.productName}.exe"
 		);
 	}
 
-	public static void BuildLinux(bool isInBuildSequence) {
+	public static void BuildLinux(bool isInBuildSequence, bool needZip) {
 		BaseBuild(
 			BuildTargetGroup.Standalone, BuildTarget.StandaloneLinux64,
 			isInBuildSequence ? BuildOptions.None : BuildOptions.ShowBuiltPlayer,
 			!isInBuildSequence,
 			!isInBuildSequence,
+			needZip ? "_Linux" : null,
 			$"_Linux/{PlayerSettings.productName}.x86_64"
 		);
 	}
 
-	public static void BuildOSX(bool isInBuildSequence) {
+	public static void BuildOSX(bool isInBuildSequence, bool needZip) {
 		BaseBuild(
 			BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX,
 			isInBuildSequence ? BuildOptions.None : BuildOptions.ShowBuiltPlayer,
 			!isInBuildSequence,
 			!isInBuildSequence,
+			needZip ? "_OSX" : null,
 			$"_OSX/{PlayerSettings.productName}"
 		);
 	}
 
-	public static void BuildWeb(bool isInBuildSequence) {
+	public static void BuildWeb(bool isInBuildSequence, bool needZip) {
 		BaseBuild(
 			BuildTargetGroup.WebGL, BuildTarget.WebGL, 
 			isInBuildSequence ? BuildOptions.None : BuildOptions.ShowBuiltPlayer, 
 			!isInBuildSequence,
 			!isInBuildSequence,
+			needZip ? "_Web" : null,
 			$"_Web"
 		);
 	}
 
-	static void BaseBuild(BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions, bool needReturnBuildTarget, bool incrementPatch, string buildName) {
+	static void BaseBuild(BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions, bool needReturnBuildTarget, bool incrementPatch, string zipPath, string buildName) {
+		string basePath = $"Builds/{PlayerSettings.productName}_{PlayerSettings.bundleVersion}.{LastBuildPatch}";
 		BuildTarget targetBeforeStart = EditorUserBuildSettings.activeBuildTarget;
 		BuildTargetGroup targetGroupBeforeStart = BuildPipeline.GetBuildTargetGroup(targetBeforeStart);
 
@@ -118,7 +123,7 @@ public static class BuildManager {
 
 		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions {
 			scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray(),
-			locationPathName = $"Builds/{PlayerSettings.productName}_{PlayerSettings.bundleVersion}.{LastBuildPatch}" + buildName,
+			locationPathName = basePath + buildName,
 			targetGroup = buildTargetGroup,
 			target = buildTarget,
 			options = buildOptions,
@@ -133,6 +138,8 @@ public static class BuildManager {
 		if (summary.result == BuildResult.Succeeded) {
 			Debug.Log($"{summary.platform} succeeded.  \t Time: {string.Format("{0:mm\\:ss}", summary.totalTime)}  \t Size: {summary.totalSize / 1048576f}");
 
+			if (zipPath != null && zipPath != "")
+				Compress(basePath + zipPath);
 		}
 		else if (summary.result == BuildResult.Failed) {
 			Debug.Log(
@@ -147,5 +154,12 @@ public static class BuildManager {
 
 		if (needReturnBuildTarget)
 			EditorUserBuildSettings.SwitchActiveBuildTarget(targetGroupBeforeStart, targetBeforeStart);
+	}
+
+	public static void Compress(string dirPath) {
+		using (ZipFile zip = new ZipFile()) {
+			zip.AddDirectory(dirPath, $"{PlayerSettings.productName}_{PlayerSettings.bundleVersion}.{LastBuildPatch}");
+			zip.Save(dirPath + ".zip");
+		}
 	}
 }
--- LD45/Assets/Scripts/Core/Editor/BuildPipeline/BuildManagerEditor.cs
+++ LD45/Assets/Scripts/Core/Editor/BuildPipeline/BuildManagerEditor.cs
@@ -8,36 +8,41 @@ using UnityEditor;
 using UnityEditor.Build.Reporting;
 
 public static class BuildManagerEditor {
+	[MenuItem("Build/Build All & ZIP")]
+	public static void BuildAllZipped() {
+		BuildManager.BuildAll(true);
+	}
+
 	[MenuItem("Build/Build All")]
 	public static void BuildAll() {
-		BuildManager.BuildAll();
+		BuildManager.BuildAll(false);
 	}
 
 	[MenuItem("Build/Build Windows")]
 	public static void BuildWindows() {
-		BuildManager.BuildWindows(false);
+		BuildManager.BuildWindows(false, false);
 	}
 
 	[MenuItem("Build/Build Windows x64")]
 	public static void BuildWindowsX64() {
-		BuildManager.BuildWindowsX64(false);
+		BuildManager.BuildWindowsX64(false, false);
 
 	}
 
 	[MenuItem("Build/Build Linux")]
 	public static void BuildLinux() {
-		BuildManager.BuildLinux(false);
+		BuildManager.BuildLinux(false, false);
 
 	}
 
 	[MenuItem("Build/Build OSX")]
 	public static void BuildOSX() {
-		BuildManager.BuildOSX(false);
+		BuildManager.BuildOSX(false, false);
 
 	}
 
 	[MenuItem("Build/Build Web")]
 	public static void BuildWeb() {
-		BuildManager.BuildWeb(false);
+		BuildManager.BuildWeb(false, false);
 	}
 }
