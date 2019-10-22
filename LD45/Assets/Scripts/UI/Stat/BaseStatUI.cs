ed48b7c6a35fa13fddcc9d0996a070c82356ace7
Team-on <timonsol13@gmail.com>
2019-10-23 01:33:25 +0300

    Stats & Stats UI

--- LD45/Assets/Scripts/UI/Stat/BaseStatUI.cs
+++ LD45/Assets/Scripts/UI/Stat/BaseStatUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,19 +8,22 @@ public class BaseStatUI : MonoBehaviour {
 	[SerializeField] Image filledImg;
 	[SerializeField] BaseStat stat;
 
+	delegate int a(int b, int a);
+	event a onval;
+
 	void Awake() {
-		//stat.OnValueChange += UpdateFill;
+		stat.OnValueChange += UpdateFill;
 	}
 
 	void Start() {
-		//UpdateFill();
+		UpdateFill();
 	}
 
 	void OnDestroy() {
-		//stat.OnValueChange -= UpdateFill;
+		stat.OnValueChange -= UpdateFill;
 	}
 
 	public void UpdateFill() {
-		//filledImg.fillAmount = stat.Value / stat.MaxValue;
+		filledImg.fillAmount = stat.Value / stat.MaxValue;
 	}
 }
