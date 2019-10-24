afd7dd800eed0bed974a773a9414ba986a40b629
Team-on <timonsol13@gmail.com>
2019-10-24 17:32:01 +0300

    Resize all sprites(/3)

--- LD45/Assets/Scripts/Game/Character/Player/Equipment.cs
+++ LD45/Assets/Scripts/Game/Character/Player/Equipment.cs
@@ -12,22 +12,22 @@ public class Equipment : MonoBehaviour {
 
 	Animator animator;
 
-    private bool IsBuildingEquip;
-    private GameObject Test;
+    //private bool IsBuildingEquip;
+    //private GameObject Test;
 
 	private void Start() {
 		animator = GameManager.Instance.Player.Animator;
 	}
 
-    private void Update() {
-        if(IsBuildingEquip) {
-			var pos = GameManager.Instance.MainCamera.ScreenToWorldPoint(Input.mousePosition);
-            Test.transform.position = pos;
-			Debug.Log("X on canvas : " + Test.transform.position.x + " Y on canvas : " + Test.transform.position.y);
-			Debug.Log("X : " + pos.x + " Y : " + pos.y);
+ //   private void Update() {
+ //       if(IsBuildingEquip) {
+	//		var pos = GameManager.Instance.MainCamera.ScreenToWorldPoint(Input.mousePosition);
+ //           Test.transform.position = pos;
+	//		Debug.Log("X on canvas : " + Test.transform.position.x + " Y on canvas : " + Test.transform.position.y);
+	//		Debug.Log("X : " + pos.x + " Y : " + pos.y);
 
-		}
-	}
+	//	}
+	//}
 
     public void PlayUseHandItemAnim() {
 		if(hands != null) {
@@ -74,7 +74,7 @@ public class Equipment : MonoBehaviour {
 			ItemInHand.transform.localScale = new Vector3(hands.ScaleInHand, hands.ScaleInHand, 1.0f);
 			ItemInHand.sprite = hands.Sprite;
 			ItemInHand.enabled = true;
-            CheckForBuilding(item);
+            //CheckForBuilding(item);
         }
 	}
 
@@ -82,23 +82,23 @@ public class Equipment : MonoBehaviour {
 		return OnUseHandAnimEndEvent != null;
 	}
 
-    private void CheckForBuilding(ItemSO item)
-    {
-		//Todo buildings
-		//1) Fix bug with don's set enable to false (invisible object let fors for Player)
-		//2) Fix bug with no visibility of creating GameObjec
-		if (item.MataType is ItemSO.ItemMetaType.Building && hands != null) {
-			IsBuildingEquip = true;
-			Test = Instantiate(BuildingsList.instance.GetItemPrefab(item), Input.mousePosition, Quaternion.identity, GameManager.Instance.CollectorBuilding.transform);
-			Test.GetComponent<BoxCollider2D>().enabled = false;
+ //   private void CheckForBuilding(ItemSO item)
+ //   {
+	//	//Todo buildings
+	//	//1) Fix bug with don's set enable to false (invisible object let fors for Player)
+	//	//2) Fix bug with no visibility of creating GameObjec
+	//	if (item.MataType is ItemSO.ItemMetaType.Building && hands != null) {
+	//		IsBuildingEquip = true;
+	//		Test = Instantiate(BuildingsList.instance.GetItemPrefab(item), Input.mousePosition, Quaternion.identity, GameManager.Instance.CollectorBuilding.transform);
+	//		Test.GetComponent<BoxCollider2D>().enabled = false;
 			
-			//Test.GetComponent<Renderer>().material.color = Color.;
-		}
-		else {
-			Debug.Log("IsBuildingEquip: " + IsBuildingEquip + " in else");
-			IsBuildingEquip = false;
-		}
-	}
+	//		//Test.GetComponent<Renderer>().material.color = Color.;
+	//	}
+	//	else {
+	//		Debug.Log("IsBuildingEquip: " + IsBuildingEquip + " in else");
+	//		IsBuildingEquip = false;
+	//	}
+	//}
 
 	public void InterruptAction() {
 		OnUseHandAnimEndEvent = null;
