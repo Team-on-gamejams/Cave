4db0b1d618be67c8ba1c60dc45a818813502cdbd
Team-on <timonsol13@gmail.com>
2019-10-25 19:50:55 +0300

    Simple zombie moving

--- LD45/Assets/Scripts/Game/Character/Enemy.cs
+++ LD45/Assets/Scripts/Game/Character/Enemy.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : Character {
+	void OnTriggerEnter(Collider other) {
+		
+	}
 
+	void OnTriggerStay2D(Collider2D collision) {
+		if(collision.tag == "Player") 
+			MoveTo(collision.transform.position);
+	}
 }
