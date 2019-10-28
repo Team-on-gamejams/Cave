5d59b1983e77f7bb3d23644848c64afe3f6412e4
Team-on <timonsol13@gmail.com>
2019-10-28 02:31:26 +0200

    Show stat numeric value when mouse over

--- LD45/Assets/Scripts/UI/Game/Stat/BaseStatUI.cs
+++ LD45/Assets/Scripts/UI/Game/Stat/BaseStatUI.cs
@@ -3,16 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
-public class BaseStatUI : MonoBehaviour {
+public class BaseStatUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 	[SerializeField] Image filledImg;
 	[SerializeField] BaseStat stat;
+	[SerializeField] TextMeshProUGUI statText;
 
 	void Awake() {
 		stat.OnValueChange += UpdateFill;
 	}
 
 	void Start() {
+		statText.gameObject.SetActive(false);
 		UpdateFill();
 	}
 
@@ -20,7 +24,17 @@ public class BaseStatUI : MonoBehaviour {
 		stat.OnValueChange -= UpdateFill;
 	}
 
+	public void OnPointerEnter(PointerEventData eventData) {
+		statText.gameObject.SetActive(true);
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		statText.gameObject.SetActive(false);
+	}
+
 	public void UpdateFill() {
 		filledImg.fillAmount = stat.Value / stat.MaxValue;
+		if (statText.gameObject.activeSelf)
+			statText.text = $"{stat.Value} / {stat.MaxValue}";
 	}
 }
