593e51f1fc540ab783d9cfaba118de725d0d2126
Team-on <timonsol13@gmail.com>
2019-10-28 19:59:07 +0200

    Bigmap controls

--- LD45/Assets/Scripts/UI/Game/BigMapUI.cs
+++ LD45/Assets/Scripts/UI/Game/BigMapUI.cs
@@ -8,29 +8,25 @@ public class BigMapUI : BaseUI, IDragHandler {
 	[SerializeField] float minZoom = 1;
 	[SerializeField] float maxZoom = 20;
 
-	const float dragMultDiv = 500;
+	[SerializeField] float WASDMoveSpeed = 20;
+	[SerializeField] float dragMultDiv = 500;
 	float dragMult;
 
 	void Start() {
 		dragMult = MapCamera.orthographicSize / dragMultDiv;
-
 		HideAfterStart();
 	}
 
 	void Update() {
-		var wheelDelta = Input.GetAxis("Mouse ScrollWheel");
-		if(wheelDelta > 0 && maxZoom > MapCamera.orthographicSize) {
-			MapCamera.orthographicSize += 1;
-			dragMult = MapCamera.orthographicSize / dragMultDiv;
-		}
-		else if (wheelDelta < 0 && minZoom < MapCamera.orthographicSize) {
-			MapCamera.orthographicSize -= 1;
-			dragMult = MapCamera.orthographicSize / dragMultDiv;
+		if (!GameManager.Instance.IsPaused) {
+			ProcessZoom();
+			ProcessWASDMove();
 		}
 	}
 
 	public void OnDrag(PointerEventData eventData) {
-		MapCamera.transform.position += (Vector3)(-eventData.delta * dragMult);
+		if (!GameManager.Instance.IsPaused) 
+			MapCamera.transform.position += (Vector3)(-eventData.delta * dragMult);
 	}
 
 	protected override void BeforeShow() {
@@ -43,5 +39,25 @@ public class BigMapUI : BaseUI, IDragHandler {
 
 	public override bool CanChangeShowHide() {
 		return !GameManager.Instance.IsPaused;
+	}
+
+	void ProcessZoom() {
+		var wheelDelta = Input.GetAxis("Mouse ScrollWheel");
+		if (wheelDelta > 0 && maxZoom > MapCamera.orthographicSize) {
+			MapCamera.orthographicSize += 1;
+			dragMult = MapCamera.orthographicSize / dragMultDiv;
+		}
+		else if (wheelDelta < 0 && minZoom < MapCamera.orthographicSize) {
+			MapCamera.orthographicSize -= 1;
+			dragMult = MapCamera.orthographicSize / dragMultDiv;
+		}
+	}
+
+	//TODO: ability to map WASD
+	void ProcessWASDMove() {
+		float v = Input.GetAxisRaw("Vertical"), h = Input.GetAxisRaw("Horizontal");
+		if (v != 0 || h != 0) {
+			MapCamera.transform.position += new Vector3(h, v).normalized * WASDMoveSpeed * Time.deltaTime;
+		}
 	}
 }
