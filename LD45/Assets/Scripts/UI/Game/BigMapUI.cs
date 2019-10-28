85e5f6df8a7245234d818197d7df0327ec268dd8
Team-on <timonsol13@gmail.com>
2019-10-28 19:14:18 +0200

    Add script for Big map

--- LD45/Assets/Scripts/UI/Game/BigMapUI.cs
+++ LD45/Assets/Scripts/UI/Game/BigMapUI.cs
@@ -1,10 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class BigMapUI : BaseUI {
+public class BigMapUI : BaseUI, IDragHandler {
+	[SerializeField] Camera MapCamera;
+	[SerializeField] float minZoom = 1;
+	[SerializeField] float maxZoom = 20;
+
+	const float dragMultDiv = 500;
+	float dragMult;
+
 	void Start() {
+		dragMult = MapCamera.orthographicSize / dragMultDiv;
+
 		HideAfterStart();
+	}
+
+	void Update() {
+		var wheelDelta = Input.GetAxis("Mouse ScrollWheel");
+		if(wheelDelta > 0 && maxZoom > MapCamera.orthographicSize) {
+			MapCamera.orthographicSize += 1;
+			dragMult = MapCamera.orthographicSize / dragMultDiv;
+		}
+		else if (wheelDelta < 0 && minZoom < MapCamera.orthographicSize) {
+			MapCamera.orthographicSize -= 1;
+			dragMult = MapCamera.orthographicSize / dragMultDiv;
+		}
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		MapCamera.transform.position += (Vector3)(-eventData.delta * dragMult);
 	}
 
 	protected override void BeforeShow() {
