65e077226d291eea0d4af23d17b81a4d0050d572
Team-on <timonsol13@gmail.com>
2019-11-05 13:40:03 +0200

    World generator base

---
+++ LD45/Assets/Scripts/Game/Map/Chunk.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour {
+	public int x, y;
+	public Chunk up, right, down, left;
+
+	public void GenerateNearbyChunks() {
+		GameObject chunkgo = new GameObject("Chunk");
+		Chunk chunk = chunkgo.AddComponent<Chunk>();
+	}
+}
---
+++ LD45/Assets/Scripts/Game/Map/WorldGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour {
+	public static WorldGenerator instance;
+
+	[SerializeField] Chunk startingChunk;
+
+	void Awake() {
+		instance = this;
+	}
+
+	void Start() {
+		startingChunk.GenerateNearbyChunks();
+	}
+
+	void OnDestroy() {
+		instance = null;
+	}
+}
