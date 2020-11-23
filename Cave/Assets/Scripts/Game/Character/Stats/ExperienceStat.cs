3bcc8f53a079d11ea3fb955eee3932840222f5a4
Team <timonsol13@gmail.com>
2020-11-23 20:59:57 +0200

    Revert "Move LD45 code to cave"
    This reverts commit 1f4d246847911b4b68bbe77cf702ff00cb8ccdc9.

--- Cave/Assets/Scripts/Game/Character/Stats/ExperienceStat.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-//TODO: абсолютно впевнений що це має бути НЕ BaseStat
-public class ExperienceStat : BaseStat {
-}
--- Cave/Assets/Scripts/UI/Game/CraftUI.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CraftUI : BaseUI {
-	void Start() {
-		HideAfterStart();
-	}
-}
